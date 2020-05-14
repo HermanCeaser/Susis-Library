e51a61754ecfc0c1285dfb0dde3642f6557d511e
Herman Ceaser <ceaserbanks09@gmail.com>
2020-05-14 15:21:06 +0300

    Changed Layout of the Page

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = require('./src/routes/bookRouter');
+const nav = [
+    {
+        link: '/books',
+        title: 'Books'
+    },
+    {
+        link: '/authors',
+        title: 'Authors'
+    },
+];
+
+const bookRouter = require('./src/routes/bookRouter')(nav);
+
 const authorRouter = express.Router();
+
 
 app.use(morgan('tiny'));
 
@@ -18,11 +31,12 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 app.use('/fonts', express.static(path.join(__dirname, '/node_modules/bootstrap/fonts/')));
+app.use('/fonts', express.static(path.join(__dirname, '/public/fonts/')));
+app.use('/images', express.static(path.join(__dirname, '/images/')));
 
 //  set app Settings
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
 
 
 authorRouter.route('/').get((req, res) => {
@@ -37,16 +51,7 @@ app.use('/authors', authorRouter);
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Library',
-        nav: [
-            {
-                link: '/books',
-                title: 'Books'
-            },
-            {
-                link: '/authors',
-                title: 'Authors'
-            },
-        ],
+        nav,
     });
 });
 
--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,85 +1,72 @@
 const express = require('express');
 
-const BookRouter = express.Router();
+const bookRouter = express.Router();
 
-const books = [
-    {
-        id: 1,
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        author: 'Kenneth Grahame',
-        read: false
-    },
-    {
-        id: 2,
-        title: 'Life On The Mississipi',
-        genre: 'History',
-        author: 'Mark Twain',
-        read: false
-    },
-    {
-        id: 3,
-        title: 'Bill  Clinton',
-        genre: 'Biography',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: true
-    },
-    {
-        id: 4,
-        title: 'Think And Grow Rich',
-        genre: 'Entrepreneurship',
-        author: 'Napolean Hill',
-        read: false
-    },
-    {
-        id: 5,
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        id: 6,
-        title: 'Rich Dad Poor Dad',
-        genre: 'Fiction',
-        author: 'Napolean Hill',
-        read: false
-    },
-];
+const router = (nav) => {
+    const books = [
+        {
+            id: 1,
+            title: 'The Wind in the Willows',
+            genre: 'Fantasy',
+            author: 'Kenneth Grahame',
+            read: false
+        },
+        {
+            id: 2,
+            title: 'Life On The Mississipi',
+            genre: 'History',
+            author: 'Mark Twain',
+            read: false
+        },
+        {
+            id: 3,
+            title: 'Bill  Clinton',
+            genre: 'Biography',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: true
+        },
+        {
+            id: 4,
+            title: 'Think And Grow Rich',
+            genre: 'Entrepreneurship',
+            author: 'Napolean Hill',
+            read: false
+        },
+        {
+            id: 5,
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            id: 6,
+            title: 'Rich Dad Poor Dad',
+            genre: 'Fiction',
+            author: 'Napolean Hill',
+            read: false
+        },
+    ];
 
-BookRouter.route('/').get((req, res) => {
-    res.render('books', {
-        books,
-        title: 'Library',
-        nav: [
-            {
-                link: '/books',
-                title: 'Books'
-            },
-            {
-                link: '/authors',
-                title: 'Authors'
-            },
-        ],
+    bookRouter.route('/').get((req, res) => {
+        res.render('books', {
+            books,
+            title: 'Library',
+            nav,
+        });
     });
-});
 
-BookRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-    res.render('book', {
-        book: books[id],
-        title: 'Library',
-        nav: [
-            {
-                link: '/books',
-                title: 'Books'
-            },
-            {
-                link: '/authors',
-                title: 'Authors'
-            },
-        ],
+    bookRouter.route('/:id').get((req, res) => {
+        const { id } = req.params;
+        res.render('book', {
+            book: books[id],
+            title: 'Library',
+            nav,
+        });
     });
-});
 
-module.exports = BookRouter;
+    return bookRouter;
+};
+
+
+module.exports = router;
